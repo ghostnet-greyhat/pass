fe03039fb58fa9900a8b0c030084f49eaece0d37
ghostnet-whitehat <117953907+ghostnet-whitehat@users.noreply.github.com>
2024-05-21 03:07:34 -0300

    Add files via upload

---
+++ pass.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# creado por ghostnet
+#
+function  pass() {
+# start colors
+
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+ORANGE="\033[38;2;255;165;0m"
+VIOLET="\033[38;5;93m"
+LILA="\033[38;5;177m"
+DARK_RED="\033[38;5;52m"
+RESET='\033[0m'
+
+# end colors
+clear
+echo -e $GREEN "sigueme en github: https://github.com/ghostnet-whitehat/"
+echo -e $ORANGE"
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣷⣄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣅⣹⣿⣷⣄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣷⣄⠈⠳⣄⠙⢿⣿⣿⣏⢙⣿⠗⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣷⣄⠙⢷⣄⠙⢿⣿⠟⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠙⠳⣄⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡈⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀RANDOM PASSWORD GENERATOR BY GHOSTNET⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠁⠀$RESET "
+# Generar contraseña aleatoria de 64 caracteres
+password=$(openssl rand -base64 64 | grep -o '[a-zA-Z0-9_-]' | head -c 78 | tr -dc '[:alnum:]!"#$%&()*+,-./:;<=>?@[]^_{|}~')
+
+# Copiar contraseña al clipboard with xclip
+xclip -sel clip <<< "$password"
+
+# Imprimir contraseña generada
+echo -e $VIOLET "Contraseña generada>>> $RESET $WHITE $password"
+echo -e $BLUE "    la contraseña fue copiada al portapapeles"
+}
+pass
